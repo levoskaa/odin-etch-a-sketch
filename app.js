4dfdd5aba48a3e6a6a5cec2263f4fc9fcb925558
levoskaa <47329402+levoskaa@users.noreply.github.com>
2023-02-06 16:56:50 +0100

    fix: preserve grid aspect ratio on resize

--- app.js
+++ app.js
@@ -15,16 +15,34 @@ function createGridCells(width, height) {
 
 function displayGrid(width, height) {
   grid.innerHTML = createGridCells(width, height);
+  grid.style.aspectRatio = gridWidth / gridHeight;
   sizeGrid();
 }
 
 function sizeGrid() {
-  const maxCellWidth = window.innerWidth / gridWidth;
-  const maxCellHeight = window.innerHeight / gridHeight;
+  const gridBorder = getBorderWidthInPx(grid);
+  const maxCellWidth = (window.innerWidth - 2 * gridBorder) / gridWidth;
+  const maxCellHeight = (window.innerHeight - 2 * gridBorder) / gridHeight;
   const cellSize = Math.min(maxCellWidth, maxCellHeight);
-  grid.style.gridTemplate = `repeat(${gridWidth}, ${cellSize}px) / repeat(${gridHeight}, ${cellSize}px)`;
+  grid.style.gridTemplateRows = `repeat(${gridHeight}, minmax(0, ${cellSize}px))`;
+  grid.style.gridTemplateColumns = `repeat(${gridWidth}, minmax(0, ${cellSize}px))`;
 }
 
+function getBorderWidthInPx(element) {
+  // Captures digits and dots (a decimal number) that come before "px"
+  // at the end of the string.
+  // Example captures:
+  // 1px -> 1,
+  // 2.85px -> 2.85,
+  // .9px -> .9
+  const widthRegex = /([\d|.]+)px$/;
+  const borderWidth = window
+    .getComputedStyle(element)
+    .borderWidth.match(widthRegex)[1];
+  return parseFloat(borderWidth);
+}
+
+// TODO: debounce
 window.addEventListener("resize", sizeGrid);
 
 displayGrid(gridWidth, gridHeight);
