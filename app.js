9cf63f796e3ad6cc79669dc08695acbf2aa5b138
levoskaa <47329402+levoskaa@users.noreply.github.com>
2023-02-06 18:15:35 +0100

    refactor: remove grid size calculation

--- app.js
+++ app.js
@@ -20,37 +20,12 @@ function displayGrid(size) {
     .forEach((cell) =>
       cell.addEventListener("mouseenter", color, { once: true })
     );
-  sizeGrid();
-}
-
-function sizeGrid() {
-  const gridBorder = getBorderWidthInPx(grid);
-  const maxCellWidth = (window.innerWidth - 2 * gridBorder) / gridSize;
-  const maxCellHeight = (window.innerHeight - 2 * gridBorder) / gridSize;
-  const cellSize = Math.min(maxCellWidth, maxCellHeight);
-  grid.style.gridTemplateRows = `repeat(${gridSize}, minmax(0, ${cellSize}px))`;
-  grid.style.gridTemplateColumns = `repeat(${gridSize}, minmax(0, ${cellSize}px))`;
-}
-
-function getBorderWidthInPx(element) {
-  // Captures digits and dots (a decimal number) that come before "px"
-  // at the end of the string.
-  // Example captures:
-  // 1px -> 1,
-  // 2.85px -> 2.85,
-  // .9px -> .9
-  const widthRegex = /([\d|.]+)px$/;
-  const borderWidth = window
-    .getComputedStyle(element)
-    .borderWidth.match(widthRegex)[1];
-  return parseFloat(borderWidth);
+  grid.style.gridTemplateRows = `repeat(${size}, auto)`;
+  grid.style.gridTemplateColumns = `repeat(${size}, auto)`;
 }
 
 function color() {
   this.classList.add("colored");
 }
-
-// TODO: debounce
-window.addEventListener("resize", sizeGrid);
 
 displayGrid(gridSize);
