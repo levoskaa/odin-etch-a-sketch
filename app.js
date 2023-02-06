eae88cf4d7508556983f8f11b21df82903f98f65
levoskaa <47329402+levoskaa@users.noreply.github.com>
2023-02-06 14:00:25 +0100

    feat: calculate grid size

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const grid = document.querySelector(".grid");
+let gridWidth = 16;
+let gridHeight = 16;
 
 function createGridCells(width, height) {
   const cells = [];
   for (let i = 0; i < width; i++) {
     for (let j = 0; j < height; j++) {
-      const cell = "<div></div>";
+      const cell = '<div class="grid-cell"></div>';
       cells.push(cell);
     }
   }
   return cells.join("");
 }
 
-grid.innerHTML = createGridCells(16, 16);
+function displayGrid(width, height) {
+  grid.innerHTML = createGridCells(width, height);
+  sizeGrid();
+}
+
+function sizeGrid() {
+  const maxCellWidth = window.innerWidth / gridWidth;
+  const maxCellHeight = window.innerHeight / gridHeight;
+  const cellSize = Math.min(maxCellWidth, maxCellHeight);
+  grid.style.gridTemplate = `repeat(${gridWidth}, ${cellSize}px) / repeat(${gridHeight}, ${cellSize}px)`;
+}
+
+window.addEventListener("resize", sizeGrid);
+
+displayGrid(gridWidth, gridHeight);
